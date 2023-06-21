c62a56a7e1747276b8b4c81191586910ec280601
matPacheco <matheus.pacheco17@outlook.com>
2023-06-20 21:41:55 -0300

    adicionado funcionalidade de search e join no server e peer e o update no server

--- ep1/main.py
+++ ep1/main.py
@@ -1,11 +1,13 @@
 import socket
+import pickle
 
 
 class Server:
     def __init__(self, port=1099):
+        # TODO retirar prints
         # Inicializar variável do socket e da peer_list
         self.socket = None
-        self.peer_list = []
+        self.peer_list = {}
 
         # Criar o socket
         try:
@@ -25,36 +27,104 @@ class Server:
 
         # Um loop infinito até que o interrompemos ou um erro ocorra
         while True:
+            # TODO verificar quando finalizar
             # Estabelecer conexão com o cliente
-            c, addr = self.socket.accept()
-            print(f"Conectado de {addr}")
+            c, address = self.socket.accept()
 
-            # Enviar uma mensagem ao client
-            # Encoding para transformar em byte
-            c.send("Obrigado por se conectar".encode())
+            # Endereço de IP e Porta do peer
+            ip_peer = address[0]
+            port_peer = address[1]
+
+            # Vamos receber uma requisição em formato de pickle com as informações
+            # Decodificamos e em "tipo" teremos qual é o tipo da requisição e chamaremos o método correspondente
+            response = pickle.loads(c.recv(4096))
+            if response["tipo"] == "JOIN":
+                self.join(c, ip_peer, port_peer, response["files_list"])
+            elif response["tipo"] == "SEARCH":
+                self.search(c, ip_peer, port_peer, response["file_name"])
+            elif response["tipo"] == "UPDATE":
+                self.update(c, ip_peer, port_peer, response["file_name"])
 
             # Encerrar a conexão com o client
             c.close()
 
-            # Interromper o while após fechar conexão
-            break
+    def join(self, client, ip_address, port, data):
+        # Adicionamos a lista de arquivos ao dicionário com a lista dos peers
+        # Usamos [IP_ADDRESS]:[PORT] como chave
+        self.peer_list[f"{ip_address}:{port}"] = data
+        print(f"Peer {ip_address}:{port} adicionado com arquivos {str(data)}.")
 
-        def join(client, address, data):
-            if type(data) == list:
-                self.peer_list.append({
-                    "ip_address": address[0],
-                    "port": address[1],
-                    "files": data
-                })
+        # Enviamos de volta uma mensagem avisando que o join foi concluído com sucesso
+        client.send("JOIN_OK".encode())
 
-                client.send("JOIN_OK")
-                client.close()
-            else:
-                client.send("Erro: Tipo de Arquivos incompatíveis.")
+    def search(self, client, ip_address, port, file_name):
+        print(f"Peer {ip_address}:{port} solicitou arquivo {file_name}")
 
-        def search(client, file_name):
-            peers_result = []
-            for peer in self.peer_list:
-                if file_name in peer["files"]:
-                    peers_result.append('peer["ip_address"]:peer["port"]')
-            client.send(peers_result)
+        peers_result = []
+        for peer in self.peer_list:  # TODO refazer esse for, agora é um dicionário
+            if file_name in peer["files"]:
+                peers_result.append('peer["ip_address"]:peer["port"]')
+
+        # Enviar a lista com os peers encodada com pickle
+        client.send(pickle.dumps(peers_result))
+
+    def update(self, client, ip_address, port, file_name):
+        # Adiciona esse arquivo na lista desse peer
+        self.peer_list[f"{ip_address}:{port}"].append(file_name)
+
+        # Enviamos de volta uma mensagem avisando que o update foi concluído com sucesso
+        client.send("UPDATE_OK".encode())
+        return
+
+
+class Peer:
+    def __init__(self, port=1099):
+        # Inicializar variável do socket e da peer_list
+        self.socket = None
+        self.port = port
+
+        # Criar o socket
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print("Socket criado com sucesso.")
+        except socket.error as error:
+            print(f"A criação do socket falhou com o erro {error}")
+
+    def join(self, files_list):
+        # TODO criar pasta para cada peer
+        self.socket.connect(('127.0.0.1', self.port))  # TODO tirar isso daqui e colocar em outro lugar
+
+        # Vamos enviar os dados como um dicionário
+        to_send_dict = {
+            "tipo": "JOIN",
+            "files_list": files_list
+        }
+
+        # Usamos o pickle para transformar os dados em bytes e os enviamos
+        self.socket.send(pickle.dumps(to_send_dict))
+
+        # Recebemos a resposta se tudo ok
+        response = self.socket.recv(2048).decode()
+
+        # Se a requisição foi concluída com sucesso, vai receber um JOIN_OK
+        if "JOIN_OK" in response:
+            print(f"Sou peer [IP]:[porta] com arquivos {str(files_list)}")  # TODO ip:porta
+
+    def search(self, file_name):
+        # TODO conectar o socket ao servidor
+        to_send_dict = {
+            "tipo": "SEARCH",
+            "file_name": file_name
+        }
+
+        # Usamos o pickle para transformar os dados em bytes e os enviamos
+        self.socket.send(pickle.dumps(to_send_dict))
+
+        # Recebemos a resposta em pickle por se tratar de uma lista
+        response = pickle.dumps(self.socket.recv(2048))
+
+        print(f"peers com arquivo solicitado: {str(response)}")
+
+    def update(self):
+        # TODO
+        return
